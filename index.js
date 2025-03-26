11f401c56b6a1247aa187658be36e362e9e9a7ac
Math3Faria <matheusteud12@gmail.com>
2025-03-26 09:54:18 -0300

    integrando math na main

---
+++ index.js
@@ -0,0 +1,6 @@
+//retorna a raiz cúbica de um número. Isto é
+console.log(Math.cbrt(64));
+//sempre arredonda para cima e retorna o menor inteiro maior ou igual a um determinado número.
+console.log(Math.ceil(0.95));
+//sempre arredonda para baixo e retorna o maior inteiro menor ou igual a um determinado número.
+console.log(Math.floor(4.75));
